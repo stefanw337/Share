e480948de357fef39c51f1d3075cb5013572a55c
stefanw337 <stefanw337@users.noreply.github.com>
2019-06-23 15:29:32 +0200

    sauce

---
+++ script.js
@@ -0,0 +1,10 @@
+var scene1 = "<div class='left'><div class='text'><h2>Der Struggle-Weg</h2><p>Alleine fahrn is doof.</p></div><img src='img/eat1.jpg' alt=''></div><div class='right'><div class='text'><h2>Der nachhltige Weg</h2><p>Hol' dir Apps wie ... und lass dich mal auf etwas Neues einladen</p></div><img src='img/eat2.jpg' alt=''></div>";
+var content = document.getElementsByName("content");
+
+
+function loadScene1(){
+  var scene1 = "";
+  document.getElementsByName("test").innerHTML = scene1;
+}
+
+var clicker1 = document.getElementsByClassName('footer');
